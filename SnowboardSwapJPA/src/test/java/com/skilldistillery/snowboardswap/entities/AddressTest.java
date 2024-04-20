f5d91ac0762c9073d21b17f0fe612a4ec9c434b1
ashlmasc <ashleymascarenas@yahoo.com>
2024-04-20 13:52:22 -0600

    AddressTest fully mapped to data in DB

--- SnowboardSwapJPA/src/test/java/com/skilldistillery/snowboardswap/entities/AddressTest.java
+++ SnowboardSwapJPA/src/test/java/com/skilldistillery/snowboardswap/entities/AddressTest.java
@@ -43,9 +43,9 @@ class AddressTest {
 	@Test
 	void test_address_entity_mapping() {
 		assertNotNull(address);
-		//assertEquals(address.getStreet(), "509 Copper Rd");
-//        assertEquals("Frisco", address.getCity());
-//        assertEquals("80443", address.getPostalCode());
-//        assertEquals("866-841-2549", address.getPhoneNumber());
+		assertEquals(address.getStreet(), "509 Copper Rd");
+		assertEquals("Frisco", address.getCity());
+        assertEquals(80443, address.getPostalCode());
+        assertEquals("866-841-2549", address.getPhoneNumber());
 	}
 }
