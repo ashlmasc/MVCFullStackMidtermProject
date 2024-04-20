8c78b8253e6631105a75398afbaf2ac78edf4c19
ashlmasc <ashleymascarenas@yahoo.com>
2024-04-20 13:22:29 -0600

    Address entity stub out and basic JUNIT test green with assertNotNull

--- SnowboardSwapJPA/src/main/java/com/skilldistillery/snowboardswap/entities/Ride.java
+++ SnowboardSwapJPA/src/main/java/com/skilldistillery/snowboardswap/entities/Ride.java
@@ -3,9 +3,7 @@ package com.skilldistillery.snowboardswap.entities;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import org.apache.logging.log4j.core.tools.picocli.CommandLine.Help.Ansi.Text;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.grammars.hql.HqlParser.DateTimeContext;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- SnowboardSwapJPA/src/test/java/com/skilldistillery/snowboardswap/entities/AddressTest.java
+++ SnowboardSwapJPA/src/test/java/com/skilldistillery/snowboardswap/entities/AddressTest.java
@@ -1,51 +1,51 @@
-//package com.skilldistillery.snowboardswap.entities;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.EntityManagerFactory;
-//import jakarta.persistence.Persistence;
-//
-//class AddressTest {
-//
-//	private static EntityManagerFactory emf;
-//	private EntityManager em;
-//	private Address address;
-//
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//		emf = Persistence.createEntityManagerFactory("SnowboardSwapJPA");
-//	}
-//
-//	@AfterAll
-//	static void tearDownAfterClass() throws Exception {
-//		emf.close();
-//	}
-//
-//	@BeforeEach
-//	void setUp() throws Exception {
-//		em = emf.createEntityManager();
-//		address = em.find(Address.class, 1);
-//	}
-//
-//	@AfterEach
-//	void tearDown() throws Exception {
-//		address = null;
-//		em.close();
-//	}
-//
-//	@Test
-//	void test_address_entity_mapping() {
-//		assertNotNull(address);
-//		//assertEquals(address.getStreet(), "509 Copper Rd");
-////        assertEquals("Frisco", address.getCity());
-////        assertEquals("80443", address.getPostalCode());
-////        assertEquals("866-841-2549", address.getPhoneNumber());
-//	}
-//}
+package com.skilldistillery.snowboardswap.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class AddressTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("SnowboardSwapJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		address = null;
+		em.close();
+	}
+
+	@Test
+	void test_address_entity_mapping() {
+		assertNotNull(address);
+		//assertEquals(address.getStreet(), "509 Copper Rd");
+//        assertEquals("Frisco", address.getCity());
+//        assertEquals("80443", address.getPostalCode());
+//        assertEquals("866-841-2549", address.getPhoneNumber());
+	}
+}
