081bc83eaafb7d261bd376456c10e239dae7fe67
Mary Hudner <Mhudner@yahoo.com>
2024-04-24 13:03:52 -0600

    added update method to rideshare and worked on jsps

--- SnowboardSwapBoot/src/main/java/com/skilldistillery/snowboardswap/controllers/RideShareController.java
+++ SnowboardSwapBoot/src/main/java/com/skilldistillery/snowboardswap/controllers/RideShareController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.snowboardswap.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,21 +26,12 @@ public class RideShareController {
 	private RideShareDAO rideShareDAO;
 	private AddressDAO addressDAO;
 	private ResortDAO resortDAO;
-	private UserDAO userDAO;
+	
 
 	public RideShareController(UserDAO userDAO, RideShareDAO rideShareDAO, AddressDAO addressDAO, ResortDAO resortDAO) {
-		this.userDAO = userDAO;
 		this.rideShareDAO = rideShareDAO;
 		this.addressDAO = addressDAO;
 		this.resortDAO = resortDAO;
-	}
-
-	@GetMapping({ "rideshare" })
-	public ModelAndView rideShare() {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("rideshare");
-
-		return mv;
 	}
 
 	@PostMapping("rideshare")
@@ -68,9 +61,76 @@ public class RideShareController {
 
 		return "rideshare";
 	}
+	
+	@GetMapping("rideshare")
+	public ModelAndView displayRides() {
+		ModelAndView mv = new ModelAndView();
+		List<Ride> rides = rideShareDAO.displayAllRides();
+		mv.addObject("allRides", rides);
+		mv.setViewName("rideshare");
+		return mv;
+	}
+	
+	@GetMapping("editRideShare")
+	public ModelAndView editRide(@RequestParam("id")int id) {
+		ModelAndView mv = new ModelAndView();
+		Ride ride = rideShareDAO.findRideById(id);
+		mv.addObject("ride", ride);
+		
+		mv.setViewName("editRideShare");
+		return mv;
+	}
+
+	@PostMapping("updateRideShare")
+	public ModelAndView updateRideShare(@RequestParam("rideShareId") int id, @RequestParam("sponsorid") int sponsorId,
+	        @RequestParam("resortid") int resortId, @RequestParam("street1") String street1,
+	        @RequestParam("city1") String city1, @RequestParam("state1") String state1,
+	        @RequestParam("street2") String street2, @RequestParam("city2") String city2,
+	        @RequestParam("state2") String state2, BindingResult result, HttpSession session) {
+
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("editRideShare");
+
+	    User loggedInUser = (User) session.getAttribute("loggedInUser");
+	    if (loggedInUser == null) {
+
+	    	mv.addObject("error", "User not logged in.");
+	        return mv;
+	    }
+
+	    Ride ride = rideShareDAO.findRideById(id);
+	    if (ride == null) {
+	        mv.addObject("error", "Ride share not found.");
+	        return mv;
+	    }
+
+	    if (ride.getUser().getId() != loggedInUser.getId()) {
+	        mv.addObject("error", "You are not authorized to update this ride share.");
+	        return mv;
+	    }
+
+	    // Update the rideshare with the new information
+	    Address departureAddress = addressDAO.createAddress(street1, city1, state1);
+	    Address arrivalAddress = addressDAO.createAddress(street2, city2, state2);
+	    ride.setDepartureAddress(departureAddress);
+	    ride.setArrivalAddress(arrivalAddress);
+	    ride.setResort(resortDAO.getResortByID(resortId));
+
+	    try {
+	        Ride updatedRide = rideShareDAO.updateRideShare(ride, id);
+	        if (updatedRide == null) {
+	            mv.addObject("error", "Failed to update ride share.");
+	            return mv;
+	        }
+	    } catch (Exception e) {
+	        mv.addObject("error", "An error occurred during the update operation.");
+	        return mv;
+	    }
+
+	    return mv;
+	}
+
+
+
 }
 
-//		@PostMapping("updateRideShare")
-//		public String updateRideShare(@RequestParam("rideShareId") int id, Model m) {
-//			
-//		}
---
+++ SnowboardSwapBoot/src/main/webapp/WEB-INF/editRideShare.jsp
@@ -0,0 +1,67 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	    <h2 class="display-4 text-center mb-4">Edit Current Ride Share</h2>
+            <form action="updateRideShare" method="post" enctype="multipart/form-data">
+                <!-- User id -->
+                <input type="hidden" name="id" value="1">
+                <input type="hidden" name="sponsorid" value="1">
+                <input type="hidden" name="resortid" value="1">
+            
+                <div class="form-group">
+                    <label for="departure">Post Id:</label>
+                    <input value="${ride.id}" type="text" id="id" name="id" required>
+                </div>
+                <div class="form-group">
+                    <label for="departure">Departure date and time:</label>
+                    <input value="${ride.departure}" type="datetime-local" id="departure" name="departure" required>
+                </div>
+
+               <!--  <div class="form-group">
+                    <label for="departureAddress">Departure Address:</label>
+                    <input type="text" name="street1">
+                    <input type="text" name="city1">
+                    <input type="text" name="state1">
+                </div>
+
+                <div class="form-group">
+                    <label for="arrivalAddress">Arrival Address:</label>
+  					 <input type="text" name="street2">
+                    <input type="text" name="city2">
+                    <input type="text" name="state2">
+                </div> -->
+                
+                <div class="form-group">
+                    <label for="detail">Details:</label>
+                    <input value="${ride.detail}" type="text" id="detail" name="detail" required>
+
+                </div>
+                
+                <div class="form-group">
+                    <label for="vehicleCapacity">Vehicle capacity:</label>
+                    <input value="${ride.vehicleCapacity}" type="number" class="form-control" id="vehicleCapacity" name="vehicleCapacity">
+                </div>
+                
+                <div class="form-group">
+                    <label for="vehicleMake">Vehicle make:</label>
+                    <input value="${ride.vehicleMake}" type="text" class="form-control" id="vehicleMake" name="vehicleMake">
+                </div>
+                
+                <div class="form-group">
+                    <label for="vehicleModel">Vehicle model:</label>
+                    <input value="${ride.vehicleModel}" type="text" class="form-control" id="vehicleModel" name="vehicleModel">
+                </div>
+                
+                <input type="submit" value="Post Edited Ride Share" class="btn btn-primary">
+            </form>
+		
+		
+</body>
+</html>
